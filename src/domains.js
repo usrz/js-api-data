437342e5ec36e2247f222687ded295a58f3e0541
Pier Fumagalli <pier@usrz.com>
2015-05-08 22:20:36 +0900

    Domains on stores.

--- src/domains.js
+++ src/domains.js
@@ -1,50 +1,31 @@
 'use strict';
 
-const DbClient = require('./db-client');
+const DbStore = require('./db-store');
 const UUID = require('./uuid');
 const util = require('util');
 
-function Domains(roUri, rwUri) {
-  if (! roUri) throw new Error('At least one URI must be specified');
-  if (! rwUri) rwUri = roUri;
+const domain_placeholder = UUID.NULL.toString();
+const domains = new WeakMap();
 
-  var roClient = new DbClient(roUri);
-  var rwClient = new DbClient(rwUri);
-
-  this.select = function select(uuid) {
-    return new Promise(function(resolve, reject) {
-      try {
-        uuid = UUID(uuid).toString();
-      } catch (error) {
-        return resolve(null);
-      }
-
-      resolve(roClient.query('SELECT * FROM "domains" WHERE "uuid" = $1', uuid)
-        .then(function(result) {
-          if (!result) throw new Error('No result from database');
-          if (result.rowCount == 0) return null;
-          if (result.rowCount == 1) return result.rows[0];
-          throw new Error('Unexpected ' + result.rowCount + ' rows from database');
-        }));
-    });
+class Domains {
+  constructor(keyManager, roClient, rwClient) {
+    domains.set(this, new DbStore("domains", keyManager, roClient, rwClient));
   }
 
-  this.insert = function insert(attributes) {
-    return new Promise(function(resolve, reject) {
-      if (!util.isObject(attributes)) throw new Error('Attributes must be an object');
-      var buffer = new Buffer(JSON.stringify(attributes), 'utf8');
+  find(uuid, include_deleted) {
+    return domains.get(this).select(uuid, include_deleted);
+  }
 
-      resolve(rwClient.query('INSERT INTO "domains" (attributes) VALUES ($1) RETURNING *', buffer)
-        .then(function(result) {
-          if (!result) throw new Error('No result from database');
-          if (result.rowCount == 0) return null;
-          if (result.rowCount == 1) {
-            result.rows[0].attributes = JSON.parse(result.rows[0].attributes.toString('utf8'));
-            return result.rows[0];
-          }
-          throw new Error('Unexpected ' + result.rowCount + ' rows from database');
-        }));
-    });
+  create(attributes) {
+    return domains.get(this).insert(domain_placeholder, attributes);
+  }
+
+  modify(uuid, attributes) {
+    return domains.get(this).update(uuid, attributes);
+  }
+
+  delete(uuid) {
+    return domains.get(this).delete(uuid);
   }
 }
 

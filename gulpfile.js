2781fad2ec8a6da40a868d6fc74d9af415787300
Pier Fumagalli <pier@usrz.com>
2015-06-08 23:23:48 +0900

    More gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
+var sequence = require('run-sequence');
 
-gulp.task('default', function () {
-    return gulp.src('test/**/*.test.js', {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({reporter: 'spec'}));
+gulp.task('eslint', function () {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(eslint({ configFile: 'eslint.json' }))
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('mocha', function () {
+  return gulp.src('test/**/*.test.js', {read: false})
+    .pipe(mocha({ reporter: 'spec' }));
+});
+
+gulp.task('default', function(done) {
+  return sequence('mocha', 'eslint', done);
 });

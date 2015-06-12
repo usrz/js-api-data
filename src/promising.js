c2582f51081ef1acbdbb7bf84811559fa8ec5ff9
Pier Fumagalli <pier@usrz.com>
2015-06-12 12:52:52 +0900

    Wrap Express router for promises

---
+++ src/promising.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const express = require('express').Router;
+const methods = require('methods').slice(0).concat('all');
+
+exports = module.exports = function(options) {
+  // Options, default "mergeParams" is true
+  if (! options) options = {};
+  if (! options.hasOwnProperty('mergeParams')) options.mergeParams = true;
+
+  // Create a router
+  let router = express(options);
+
+  // Override our methods
+  methods.forEach(function(method) {
+    let handler = router[method];
+    if (typeof handler !== 'function') return;
+    router[method] = function() {
+
+      // Get called only with one argument, return setting!
+      if ((method === 'get') && (arguments.length === 1)) {
+        return handler.call(router, arguments[0]);
+      }
+
+      // Wrap any other callback
+      let args = [ arguments[0] ]; // start with the path
+      let callbacks = Array.prototype.slice.call(arguments, 1);
+
+      callbacks.forEach(function(callback) {
+        args.push(function(req, res, next) {
+          try {
+            var promise = callback(req, res, next);
+            if (promise) {
+              if (typeof promise.catch === 'function') promise.catch(next);
+              if (typeof promise.then === 'function') {
+                promise.then(function(result) {
+                  if (result) {
+                    try {
+                      res.send(result).end();
+                    } catch (error) {
+                      next(error);
+                    }
+                  }
+                });
+              }
+            }
+          } catch (error) {
+            next(error);
+          }
+        });
+      });
+
+      // Invoke the original handler
+      return handler.apply(router, args);
+    };
+  });
+
+  // Return the router
+  return router;
+};

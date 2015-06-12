2d76836fbe02a974dd10a9fa2043e3cfadea6791
Pier Fumagalli <pier@usrz.com>
2015-06-12 20:56:20 +0900

    This seems to be our best approach for APIs.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var DbClient = require('./src/db-client.js');
 var KeyManager = require('./src/key-manager.js');
@@ -6,12 +8,38 @@ var client = new DbClient('postgres://127.0.0.1/usrz')
 var keyManager = new KeyManager(new Buffer(32).fill(0), client);
 
 var domains = require('./src/api-domains.js')(keyManager, client);
+const bodyParser = require('body-parser');
 
 var app = express();
+
+// No etag, x-powered-by
+app.set('etag', false);
+app.set('x-powered-by', false);
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+// The root of all evil...
 app.use('/domains', domains);
 
-listener = app.listen(8080, '127.0.0.1', function(error) {
+// Our error handler
+app.use(function(err, req, res, next) {
+  console.log("ERROR", err.stack || err);
+  if (typeof err.status === 'number') {
+    res.status(err.status);
+  } else {
+    res.status(500);
+  }
+  if (typeof err.toJSON === 'function') {
+    res.json(err).end();
+  } else {
+    res.end();
+  }
+});
+
+// Listen!
+var listener = app.listen(8080, '127.0.0.1', function(error) {
   if (error) done(error);
   var address = listener.address();
-  console.log('Running at http://' + address.address + ':' + address.port + '/login');
+  console.log('Running at http://' + address.address + ':' + address.port + '/');
 });
--- src/api-domains.js
+++ src/api-domains.js
@@ -2,38 +2,65 @@
 
 /// 409 -> Conflict: resource already exist!
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 const Domains = require('./domains.js');
+const S = require('express-statuses');
+const promising = require('./promising.js');
+
+const DbStore = require('./db-store');
+const joi = require('joi');
+
+const schema = joi.object({
+    name:        joi.string().required().replace(/\s+/g, ' ').trim().min(1).max(1024),
+    domain_name: joi.string().required().regex(/^(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}$/i)
+});
+
+function validator(attributes) {
+  var result = joi.validate(attributes, schema, { abortEarly: false });
+  if (result.error) {
+    console.log('ERROR', result.error);
+    //return Promise.reject(result.error);
+    throw new S.BAD_REQUEST({error: result.error, details: result.error.details});
+  }
+  return result.value;
+}
 
 exports = module.exports = function(keyManager, client) {
 
-  let app = express();
-  let domains = new Domains(keyManager, client);
+  let app = promising();
+  let dbstore = new DbStore(keyManager, client, validator);
+  //, 'domain'
+  let domains = new DbStore.Simple(dbstore, 'domain'); //Domains(keyManager, client);
 
-  app.use(bodyParser.json());
-
-  app.post('/', function(req, res, next) {
-    console.log('BODY', req.body);
-    return domains.create(req.body)
+  app.post('/', function(req, res) {
+    console.log('BODY', req.originalUrl, req.body);
+    return domains.create(null, req.body)
       .then(function(domain) {
-        if (! domain) return null;
-        res.header('Last-Modified', domain.updated_at);
-        res.header('Location', req.originalUrl + domain.uuid);
+        if (! domain) throw new S.NOT_FOUND();
+
+        res.header('Last-Modified', domain.updated_at.toUTCString());
+        res.header('Location', path.join(req.originalUrl, domain.uuid));
         return domain.attributes();
       });
   });
 
-  app.get('/:uuid', function(req, res, next) {
-    domains.get(req.params.uuid)
+  app.get('/:uuid', function(req, res) {
+    return domains.get(req.params.uuid)
       .then(function(domain) {
-        if (! domain) return null;
-
-        res.header('Last-Modified', domain.updated_at);
+        if (! domain) throw new S.NOT_FOUND();
+        res.header('Last-Modified', domain.updated_at.toUTCString());
         return domain.attributes();
-      })
-      .catch(next);
+      });
+  });
+
+  app.put('/:uuid', function(req, res) {
+    return domains.modify(req.params.uuid, req.body)
+      .then(function(domain) {
+        if (! domain) throw new S.NOT_FOUND();
+        res.header('Last-Modified', domain.updated_at.toUTCString());
+        return domain.attributes();
+      });
   });
 
   return app;
-}
+};
